10546a872a0aac54742faa6be64c615ed511004d
Parth Mehta <parth@id8labs.com>
2021-11-10 10:11:41 +0530

    Simple AR App for reference from https://github.com/Hariofspades/ARExperiments

---
+++ app/src/main/java/com/sample/samplearpaint/ARActivity.kt
@@ -0,0 +1,139 @@
+package com.sample.samplearpaint
+
+import android.graphics.BitmapFactory
+import android.graphics.Color
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.MaterialFactory
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ShapeFactory
+import com.google.ar.sceneform.rendering.Texture
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import com.sample.samplearpaint.R
+import kotlinx.android.synthetic.main.activity_ar.*
+
+/**
+ * Activity contatining Sceneform fragment, renders shape on command form
+ * the previous screen
+ */
+class ARActivity : AppCompatActivity() {
+
+    lateinit var fragment: ArFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar)
+
+        fragment = supportFragmentManager
+                .findFragmentById(R.id.sceneform_fragment)
+                as ArFragment
+
+        fragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+
+            when (intent.extras?.getInt("order")) {
+
+                1 -> makeSphere(hitResult, Color.BLUE)
+
+                2 ->  makeCylinder(hitResult, Color.GREEN)
+
+                3 -> makeCube(hitResult, Color.RED)
+
+                4 -> makeTextureSphere(hitResult, R.drawable.sun)
+            }
+
+        }
+    }
+
+    /**
+     * Constructs sphere of radius 1f and at position 0.0f, 0.15f, 0.0f and with TEXTURE
+     * @param hitResult - If the hit result is a plane
+     * @param res - Image res for texture, here [R.drawable.sun]
+     */
+    private fun makeTextureSphere(hitResult: HitResult, res: Int) {
+        Texture.builder().setSource(BitmapFactory.decodeResource(resources, res))
+                .build()
+                .thenAccept {
+                    MaterialFactory.makeOpaqueWithTexture(this, it)
+                            .thenAccept { material ->
+                                addNodeToScene(fragment, hitResult.createAnchor(),
+                                        ShapeFactory.makeSphere(0.1f, Vector3(0.0f, 0.15f, 0.0f), material))
+
+                            }
+                }
+    }
+
+    /**
+     * Constructs sphere of radius 1f and at position 0.0f, 0.15f, 0.0f on the plane
+     * @param hitResult - If the hit result is a plane
+     * @param res - Color
+     */
+    private fun makeSphere(hitResult: HitResult, color: Int) {
+        MaterialFactory.makeOpaqueWithColor(this,
+                com.google.ar.sceneform.rendering.Color(color))
+                .thenAccept { material ->
+                    addNodeToScene(fragment, hitResult.createAnchor(),
+                            ShapeFactory.makeSphere(0.1f, Vector3(0.0f, 0.15f, 0.0f), material))
+
+                }
+    }
+
+    /**
+     * Constructs cylinder of radius 1f and at position 0.0f, 0.15f, 0.0f on the plane
+     * Need to mention height for the cylinder
+     * @param hitResult - If the hit result is a plane
+     * @param res - Color
+     */
+    private fun makeCylinder(hitResult: HitResult, color: Int) {
+        MaterialFactory.makeOpaqueWithColor(this,
+                com.google.ar.sceneform.rendering.Color(color))
+                .thenAccept { material ->
+                    addNodeToScene(fragment, hitResult.createAnchor(),
+                            ShapeFactory.makeCylinder(0.1f, 0.3f, Vector3(0.0f, 0.15f, 0.0f), material))
+
+                }
+    }
+
+    /**
+     * Constructs cube of radius 1f and at position 0.0f, 0.15f, 0.0f on the plane
+     * Here Vector3 takes up the size - 0.2f, 0.2f, 0.2f
+     * @param hitResult - If the hit result is a plane
+     * @param res - Color
+     */
+    private fun makeCube(hitResult: HitResult, color: Int) {
+        MaterialFactory.makeOpaqueWithColor(this,
+                com.google.ar.sceneform.rendering.Color(color))
+                .thenAccept { material ->
+                    addNodeToScene(fragment, hitResult.createAnchor(),
+                            ShapeFactory.makeCube(Vector3(0.2f, 0.2f, 0.2f), Vector3(0.0f, 0.15f, 0.0f), material))
+
+                }
+    }
+
+
+    /**
+     * Adds node to the scene and the object.
+     * @param fragment - sceneform fragment
+     * @param anchor - created anchor at the tapped position
+     * @param modelObject - rendered object
+     */
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, modelObject: ModelRenderable) {
+
+        val anchorNode = AnchorNode(anchor)
+
+        TransformableNode(fragment.transformationSystem).apply {
+            renderable = modelObject
+            setParent(anchorNode)
+            select()
+        }
+
+        fragment.arSceneView.scene.addChild(anchorNode)
+    }
+}
---
+++ app/src/main/java/com/sample/samplearpaint/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.sample.samplearpaint
+
+import android.content.Intent
+import android.os.Bundle
+
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sample.samplearpaint.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+
+            R.id.sphere -> startARactivity(1)
+
+            R.id.cylinder -> startARactivity(2)
+
+            R.id.cube -> startARactivity(3)
+
+            R.id.texture -> startARactivity(4)
+        }
+    }
+
+    private fun startARactivity(i: Int) {
+        val intent = Intent(this, ARActivity::class.java)
+        intent.putExtra("order", i)
+        startActivity(intent)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        clickListeners()
+
+    }
+
+    private fun clickListeners() {
+        sphere.setOnClickListener(this)
+        cylinder.setOnClickListener(this)
+        cube.setOnClickListener(this)
+        texture.setOnClickListener(this)
+    }
+
+}
